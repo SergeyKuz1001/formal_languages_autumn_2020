5e93f53e54cb07f3a930eb067c4c0e378d19e8bc
Kuzivanov Sergey <rep78398@gmail.com>
2020-09-30 13:33:27 +0200

    Fix RPQ test.py

--- tests/rpq/test.py
+++ tests/rpq/test.py
@@ -43,14 +43,14 @@ def test_small_test(dir_name):
                 for regex in ['a | b', 'a* (b | $)', '(a | b) . (b* | a b)']])
 def test_big_test(count_vertexes, regex):
     count_edges = random.randint(1, count_vertexes ** 2)
-    I = [random.randint(0, count_vertexes) for _ in range(count_edges)]
-    J = [random.randint(0, count_vertexes) for _ in range(count_edges)]
+    I = [random.randint(0, count_vertexes - 1) for _ in range(count_edges)]
+    J = [random.randint(0, count_vertexes - 1) for _ in range(count_edges)]
     V = [random.choice(['a', 'b', 'c']) for _ in range(count_edges)]
     max_count_input_vertexes = random.randint(1, count_vertexes)
     max_count_output_vertexes = random.randint(1, count_vertexes)
-    input_vertexes = list({random.randint(0, count_vertexes)
+    input_vertexes = list({random.randint(0, count_vertexes - 1)
         for _ in range(max_count_input_vertexes)})
-    output_vertexes = list({random.randint(0, count_vertexes)
+    output_vertexes = list({random.randint(0, count_vertexes - 1)
         for _ in range(max_count_output_vertexes)})
     config = Config.from_dict(
         {
