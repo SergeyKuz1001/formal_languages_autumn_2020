29d3b91940cd34172c2febe15ac40a78ecd7a42c
Kuzivanov Sergey <rep78398@gmail.com>
2020-10-06 10:05:56 +0200

    Update meaning of ContextFreeQuery

--- src/context_free_query.py
+++ src/context_free_query.py
@@ -19,62 +19,31 @@ from typing import Dict, Set, Optional, Tuple
 
 class ContextFreeQuery(Query):
     def __init__(self) -> None:
-        self._cnf: Optional[CFG] = None
-        self._start_V: Variable = Variable('S')
+        self._cfg: Optional[CFG] = None
         self._generate_eps: Optional[bool] = None
-        self._simple_APs: Optional[Dict[Terminal, Set[Variable]]] = None
-        self._complex_APs: \
-                Optional[Dict[Tuple[Variable, Variable], Set[Variable]]] = None
-        self._complex_Ps: \
-                Optional[Dict[Variable, Set[Tuple[Variable, Variable]]]] = None
 
     @property
     def start_symbol(self) -> Variable:
-        return self._start_V
+        return self._cfg.start_symbol
 
     @property
     def variables(self) -> Set[Variable]:
-        return self._cnf.variables
+        return self._cfg.variables
 
     @property
     def terminals(self) -> Set[Terminal]:
-        return self._cnf.terminals
+        return self._cfg.terminals
 
     @property
     def generate_epsilon(self) -> Optional[bool]:
+        if self._generate_eps is None:
+            self._generate_eps = self._cfg.generate_epsilon()
         return self._generate_eps
-
-    @property
-    def simple_antiproductions(self) -> Dict[Terminal, Set[Variable]]:
-        if self._simple_APs is None:
-            self._eval_APs()
-        return self._simple_APs
-
-    @property
-    def complex_antiproductions(self) -> Dict[
-                                                 Tuple[Variable, Variable],
-                                                 Set[Variable]
-                                             ]:
-        if self._complex_APs is None:
-            self._eval_APs()
-        return self._complex_APs
-
-    @property
-    def complex_productions(self) -> Dict[
-                                             Variable,
-                                             Set[Tuple[Variable, Variable]]
-                                         ]:
-        if self._complex_Ps is None:
-            self._eval_Ps()
-        return self._complex_Ps
 
     @classmethod
     def from_text(cls, text: str) -> "ContextFreeQuery":
         res = cls()
-        cfg = CFG.from_text(text)
-        cnf = cfg.to_normal_form()
-        res._cnf = cnf
-        res._generate_eps = cfg.generate_epsilon()
+        res._cfg = CFG.from_text(text)
         return res
 
     @classmethod
@@ -91,26 +60,3 @@ class ContextFreeQuery(Query):
             lines
           ))
         return cls.from_text(text)
-
-    def _eval_APs(self) -> None:
-        self._simple_APs = dict()
-        self._complex_APs = dict()
-        for production in self._cnf.productions:
-            var = production.head
-            if len(production.body) == 1:
-                term = production.body[0]
-                self._simple_APs.setdefault(term, set())
-                self._simple_APs[term].add(var)
-            elif len(production.body) == 2:
-                var1, var2 = production.body
-                self._complex_APs.setdefault((var1, var2), set())
-                self._complex_APs[(var1, var2)].add(var)
-
-    def _eval_Ps(self) -> None:
-        self._complex_Ps = dict()
-        for production in self._cnf.productions:
-            var = production.head
-            if len(production.body) == 2:
-                var1, var2 = production.body
-                self._complex_Ps.setdefault(var, set())
-                self._complex_Ps[var].add((var1, var2))
